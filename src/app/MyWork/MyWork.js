97e37e83c05e6fd113eeb28165b9cbe4f75e3a92
MarthoxGT <43354689+MarthoxGT@users.noreply.github.com>
2022-05-04 08:52:00 -0500

    Stylize MyWork component

--- src/app/MyWork/MyWork.js
+++ src/app/MyWork/MyWork.js
@@ -2,11 +2,13 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import WorkSlider from "../../components/WorkSlider/WorkSlider";
 
+import "./myWork.css";
+
 const MyWork = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
+    <div className="myWork">
       <WorkSlider />
     </div>
   );
--- src/components/WorkSlider/WorkSlider.js
+++ src/components/WorkSlider/WorkSlider.js
@@ -1,34 +1,37 @@
 import React, { useEffect, useState } from "react";
+import "./workSlider.css";
 
 const WorkSlider = () => {
   const [projects, setProjects] = useState([]);
 
-  const { githubAPI } = require("../../assets/constants/urls.json");
+  useEffect(() => {
+    const { githubAPI } = require("../../assets/constants/urls.json");
 
-  const getProjects = () =>
     fetch(githubAPI.getRepos)
       .then((response) => response.json())
       .then((data) => setProjects(data));
+    const incrementSliderInterval = setInterval(() => {}, 3000);
 
-  useEffect(() => {
-    getProjects();
+    return () => clearInterval(incrementSliderInterval);
   }, []);
 
   return (
-    <nav>
+    <nav className="workCarrousel">
       {projects?.map((project) => (
-        <div
-          key={project.id}
-          // onClick={() => (window.location.href = project.html_url)}
-        >
-          <img src={project.owner.avatar_url} />
-          <section>
-            <h1>{project.name}</h1>
-            <h2>{project.owner.login}</h2>
-            <h3>{project.created_at}</h3>
-            <h3>{project.updated_at}</h3>
-            <p>{project.description}</p>
-          </section>
+        <div className="workCard" key={project.id}>
+          <div
+            className="cardContent"
+            onClick={() => (window.location.href = project.html_url)}
+          >
+            <img src={project.owner.avatar_url} />
+            <section>
+              <h1>{project.name}</h1>
+              <h2>{project.owner.login}</h2>
+              <h3>{project.created_at}</h3>
+              <h3>{project.updated_at}</h3>
+              <p>{project.description}</p>
+            </section>
+          </div>
         </div>
       ))}
     </nav>
