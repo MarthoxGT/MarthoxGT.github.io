c1a77da7015ea06d6dcb8f8ee5bf3e7ace7d286e
MarthoxGT <43354689+MarthoxGT@users.noreply.github.com>
2022-05-05 21:56:00 -0500

    Format date and resize photo to a better looking project card

--- src/components/WorkSlider/WorkSlider.js
+++ src/components/WorkSlider/WorkSlider.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Temporal } from "@js-temporal/polyfill";
 import "./workSlider.css";
 
 const WorkSlider = () => {
@@ -26,21 +27,47 @@ const WorkSlider = () => {
             className="cardContent"
             onClick={() => (window.location.href = project.html_url)}
           >
-            <img src={project.owner.avatar_url} />
             <section className="cardSection">
-              <h1>{project.name}</h1>
-              <h2>{project.owner.login}</h2>
+              <div className="cardHeader">
+                <img src={project.owner.avatar_url} />
+                <div className="headerContainer">
+                  <h1 className="projectTitle">{project.name}</h1>
+                  <h2 className="projectOwner">{project.owner.login}</h2>
+                </div>
+              </div>
               <div className="cardElement">
                 <h3>{t("myWork.createdAt")}</h3>
-                <p>{project.created_at}</p>
+                <p>
+                  {Temporal.Instant.from(project.created_at)
+                    .until(Temporal.Now.instant())
+                    .round("days")
+                    .total("days")}{" "}
+                  {t("myWork.daysAgo")} (
+                  {Temporal.Instant.from(project.created_at)
+                    .toString()
+                    .split("T")
+                    .at(0)}
+                  )
+                </p>
               </div>
               <div className="cardElement">
                 <h3>{t("myWork.updatedAt")}</h3>
-                <p>{project.updated_at}</p>
+                <p>
+                  {Temporal.Instant.from(project.updated_at)
+                    .until(Temporal.Now.instant())
+                    .round("days")
+                    .total("days")}{" "}
+                  {t("myWork.daysAgo")} (
+                  {Temporal.Instant.from(project.updated_at)
+                    .toString()
+                    .split("T")
+                    .at(0)}
+                  )
+                </p>
               </div>
               <div className="cardElement">
                 <h3>{t("myWork.description")}</h3>
-                <p>{project.description || t("myWork.no_description")}</p>
+                <p>{project.description || t("myWork.noDescription")}</p>
               </div>
             </section>
           </div>
