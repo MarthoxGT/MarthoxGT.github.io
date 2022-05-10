65d4f9e06acad247a5705cae30e4e2e136e6a613
MarthoxGT <43354689+MarthoxGT@users.noreply.github.com>
2022-05-09 23:24:47 -0500

    Add search bar to my work component

--- src/components/WorkSlider/WorkSlider.js
+++ src/components/WorkSlider/WorkSlider.js
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import { Temporal } from "@js-temporal/polyfill";
 import "./workSlider.css";
 
-const WorkSlider = () => {
+const WorkSlider = ({ searchQuery }) => {
   const { t } = useTranslation();
 
   const [projects, setProjects] = useState([]);
@@ -18,52 +18,54 @@ const WorkSlider = () => {
 
   return (
     <nav className="workCarrousel">
-      {projects?.map((project) => (
-        <div className="workCard" key={project.id}>
-          <section
-            className="cardContent"
-            onClick={() => (window.location.href = project.html_url)}
-          >
-            <h1>{project.name}</h1>
-            <h2>{project.owner.login}</h2>
-            <div className="cardSeparator" />
-            <div className="cardElement">
-              <h3>{t("myWork.createdAt")}</h3>
-              <p>
-                {Temporal.Instant.from(project.created_at)
-                  .until(Temporal.Now.instant())
-                  .round("days")
-                  .total("days")}{" "}
-                {t("myWork.daysAgo")} (
-                {Temporal.Instant.from(project.created_at)
-                  .toString()
-                  .split("T")
-                  .at(0)}
-                )
-              </p>
-            </div>
-            <div className="cardElement">
-              <h3>{t("myWork.updatedAt")}</h3>
-              <p>
-                {Temporal.Instant.from(project.updated_at)
-                  .until(Temporal.Now.instant())
-                  .round("days")
-                  .total("days")}{" "}
-                {t("myWork.daysAgo")} (
-                {Temporal.Instant.from(project.updated_at)
-                  .toString()
-                  .split("T")
-                  .at(0)}
-                )
-              </p>
-            </div>
-            <div className="cardElement">
-              <h3>{t("myWork.description")}</h3>
-              <p>{project.description || t("myWork.noDescription")}</p>
-            </div>
-          </section>
-        </div>
-      ))}
+      {projects
+        ?.filter((project) => project.name.includes(searchQuery))
+        .map((project) => (
+          <div className="workCard" key={project.id}>
+            <section
+              className="cardContent"
+              onClick={() => (window.location.href = project.html_url)}
+            >
+              <h1>{project.name}</h1>
+              <h2>{project.owner.login}</h2>
+              <div className="cardSeparator" />
+              <div className="cardElement">
+                <h3>{t("myWork.createdAt")}</h3>
+                <p>
+                  {Temporal.Instant.from(project.created_at)
+                    .until(Temporal.Now.instant())
+                    .round("days")
+                    .total("days")}{" "}
+                  {t("myWork.daysAgo")} (
+                  {Temporal.Instant.from(project.created_at)
+                    .toString()
+                    .split("T")
+                    .at(0)}
+                  )
+                </p>
+              </div>
+              <div className="cardElement">
+                <h3>{t("myWork.updatedAt")}</h3>
+                <p>
+                  {Temporal.Instant.from(project.updated_at)
+                    .until(Temporal.Now.instant())
+                    .round("days")
+                    .total("days")}{" "}
+                  {t("myWork.daysAgo")} (
+                  {Temporal.Instant.from(project.updated_at)
+                    .toString()
+                    .split("T")
+                    .at(0)}
+                  )
+                </p>
+              </div>
+              <div className="cardElement">
+                <h3>{t("myWork.description")}</h3>
+                <p>{project.description || t("myWork.noDescription")}</p>
+              </div>
+            </section>
+          </div>
+        ))}
     </nav>
   );
 };
