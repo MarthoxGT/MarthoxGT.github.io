5e6bf3396e4e9481b2259fd29fc6a45a96f50174
MarthoxGT <43354689+MarthoxGT@users.noreply.github.com>
2022-05-09 19:12:02 -0500

    Remodel my work style to vertical

--- src/app/MyWork/MyWork.js
+++ src/app/MyWork/MyWork.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import WorkSlider from "../../components/WorkSlider/WorkSlider";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 import "./myWork.css";
 
 const MyWork = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   return (
     <div className="myWork">
+      <SearchBar />
       <WorkSlider />
     </div>
   );
