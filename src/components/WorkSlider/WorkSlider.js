35e92b7caa15bc3c2d2e8315a5be0ad227881dc5
MarthoxGT <43354689+MarthoxGT@users.noreply.github.com>
2022-05-10 09:14:17 -0500

    Code improvements, using destructuring on functions and open project urls in a new tab

--- src/components/WorkSlider/WorkSlider.js
+++ src/components/WorkSlider/WorkSlider.js
@@ -19,53 +19,63 @@ const WorkSlider = ({ searchQuery }) => {
   return (
     <nav className="workCarrousel">
       {projects
-        ?.filter((project) => project.name.includes(searchQuery))
-        .map((project) => (
-          <div className="workCard" key={project.id}>
-            <section
-              className="cardContent"
-              onClick={() => (window.location.href = project.html_url)}
-            >
-              <h1>{project.name}</h1>
-              <h2>{project.owner.login}</h2>
-              <div className="cardSeparator" />
-              <div className="cardElement">
-                <h3>{t("myWork.createdAt")}</h3>
-                <p>
-                  {Temporal.Instant.from(project.created_at)
-                    .until(Temporal.Now.instant())
-                    .round("days")
-                    .total("days")}{" "}
-                  {t("myWork.daysAgo")} (
-                  {Temporal.Instant.from(project.created_at)
-                    .toString()
-                    .split("T")
-                    .at(0)}
-                  )
-                </p>
-              </div>
-              <div className="cardElement">
-                <h3>{t("myWork.updatedAt")}</h3>
-                <p>
-                  {Temporal.Instant.from(project.updated_at)
-                    .until(Temporal.Now.instant())
-                    .round("days")
-                    .total("days")}{" "}
-                  {t("myWork.daysAgo")} (
-                  {Temporal.Instant.from(project.updated_at)
-                    .toString()
-                    .split("T")
-                    .at(0)}
-                  )
-                </p>
-              </div>
-              <div className="cardElement">
-                <h3>{t("myWork.description")}</h3>
-                <p>{project.description || t("myWork.noDescription")}</p>
-              </div>
-            </section>
-          </div>
-        ))}
+        ?.filter(({ name }) => name.includes(searchQuery))
+        .map(
+          ({
+            id,
+            html_url,
+            name,
+            owner,
+            created_at,
+            updated_at,
+            description,
+          }) => (
+            <div className="workCard" key={id}>
+              <section
+                className="cardContent"
+                onClick={() => window.open(html_url, "_blank").focus()}
+              >
+                <h1>{name}</h1>
+                <h2>{owner.login}</h2>
+                <div className="cardSeparator" />
+                <div className="cardElement">
+                  <h3>{t("myWork.createdAt")}</h3>
+                  <p>
+                    {Temporal.Instant.from(created_at)
+                      .until(Temporal.Now.instant())
+                      .round("days")
+                      .total("days")}{" "}
+                    {t("myWork.daysAgo")} (
+                    {Temporal.Instant.from(created_at)
+                      .toString()
+                      .split("T")
+                      .at(0)}
+                    )
+                  </p>
+                </div>
+                <div className="cardElement">
+                  <h3>{t("myWork.updatedAt")}</h3>
+                  <p>
+                    {Temporal.Instant.from(updated_at)
+                      .until(Temporal.Now.instant())
+                      .round("days")
+                      .total("days")}{" "}
+                    {t("myWork.daysAgo")} (
+                    {Temporal.Instant.from(updated_at)
+                      .toString()
+                      .split("T")
+                      .at(0)}
+                    )
+                  </p>
+                </div>
+                <div className="cardElement">
+                  <h3>{t("myWork.description")}</h3>
+                  <p>{description || t("myWork.noDescription")}</p>
+                </div>
+              </section>
+            </div>
+          )
+        )}
     </nav>
   );
 };
